f23dd4947b5faa16d0d9482d2049e1cd8c46d846
jacobbogers <jkfbogers@gmail.com>
2020-03-06 12:45:23 +0100

    export tokens

--- packages/filepath/index.js
+++ packages/filepath/index.js
@@ -3,5 +3,17 @@ const { inferPathType, lexPath, resolve } = require('./lib/parser');
 module.exports = {
     inferPathType,
     lexPath,
-    resolve
+    resolve,
+    $tokens: {
+        root: {
+            POSIX_ROOT: '\x02', // done
+            TDP_ROOT: '\x03', // traditional dos path
+            UNC_ROOT: '\x04', // unc root
+            DDP_ROOT: '\x05' // dos device path root
+        },
+        SEP: '\x01', // done
+        PATHELT: '\x06',
+        PARENT: '\x07',
+        CURRENT: '\x08'
+    }
 };
